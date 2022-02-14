70c9ad88cdaa4790daab5c5e9d49c5b80eaa043f
Sanae <atmatm6000@outlook.com>
2022-02-14 16:09:55 -0600

    it doesn't work but here's a patchfix

--- Shared/Packet/Packets/ConnectPacket.cs
+++ Shared/Packet/Packets/ConnectPacket.cs
@@ -6,7 +6,7 @@ namespace Shared.Packet.Packets;
 [Packet(PacketType.Connect)]
 public struct ConnectPacket : IPacket {
     public ConnectionTypes ConnectionType;
-    public string ClientName;
+    public string ClientName = "?????";
 
     public void Serialize(Span<byte> data) {
         MemoryMarshal.Write(data, ref ConnectionType);
