a0a53ff7bf889c882f8d9718914711e2b46f54f4
Sanae <atmatm6000@outlook.com>
2022-04-02 20:58:57 -0600

    ReuseAddress requires Socket level

--- Server/Server.cs
+++ Server/Server.cs
@@ -17,7 +17,7 @@ public class Server {
 
     public async Task Listen(CancellationToken? token = null) {
         Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        serverSocket.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.ReuseAddress, true);
+        serverSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
         serverSocket.Bind(new IPEndPoint(IPAddress.Parse(Settings.Instance.Server.Address), Settings.Instance.Server.Port));
         serverSocket.Listen();
 
