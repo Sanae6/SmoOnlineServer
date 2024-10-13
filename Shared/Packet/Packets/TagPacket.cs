f324c9d8118f2cb3e96a38a8f5df3be64ed4862f
Robin C. Ladiges <rcl.git@blackpinguin.de>
2024-10-13 06:54:30 +0200

    fix: only parse time & seeking from TagPackets for Hide&Seek or Sardines
    Freeze-Tag uses the same `UpdateType` flags for some of its `TagPacket`s as H&S and Sardines do.
    But Freeze-Tag has another packet data structure that isn't matching IsIt, Seconds and Minutes.
    Therefore the server wrongly interprets Freeze-Tag packets and updates its "seeking" and "time" metadata wrongly.
    (These metadata fields are used to resend the `TagPacket` to later joining clients.)
    
    This commit does the following changes:
    - add: game mode detection in the first 4 bit of the `TagPacket.UpdateType`
    - change: parse the `TagPacket` for the metadata iff it is clearly for H&S or Sardines
    - change: only resend `TagPacket` to new clients for H&S or Sardines

--- Shared/Packet/Packets/TagPacket.cs
+++ Shared/Packet/Packets/TagPacket.cs
@@ -4,6 +4,7 @@ namespace Shared.Packet.Packets;
 
 [Packet(PacketType.Tag)]
 public struct TagPacket : IPacket {
+    public GameMode GameMode;
     public TagUpdate UpdateType;
     public bool IsIt;
     public byte Seconds;
@@ -12,22 +13,58 @@ public struct TagPacket : IPacket {
     public short Size => 5;
 
     public void Serialize(Span<byte> data) {
-        MemoryMarshal.Write(data, ref UpdateType);
+        byte both = (byte)((byte) UpdateType | ((byte) GameMode << 4));
+        MemoryMarshal.Write(data,      ref both);
         MemoryMarshal.Write(data[1..], ref IsIt);
         MemoryMarshal.Write(data[2..], ref Seconds);
         MemoryMarshal.Write(data[3..], ref Minutes);
     }
 
     public void Deserialize(ReadOnlySpan<byte> data) {
-        UpdateType = MemoryMarshal.Read<TagUpdate>(data);
-        IsIt = MemoryMarshal.Read<bool>(data[1..]);
-        Seconds = MemoryMarshal.Read<byte>(data[2..]);
-        Minutes = MemoryMarshal.Read<ushort>(data[3..]);
+        byte both  = MemoryMarshal.Read<byte>(data);
+        GameMode   = (GameMode) (sbyte) (((((both & (byte) 0xf0) >> 4) + 1) % 16) - 1);
+        UpdateType = (TagUpdate) (byte) (both & (byte) 0x0f);
+        IsIt       = MemoryMarshal.Read<bool>(data[1..]);
+        Seconds    = MemoryMarshal.Read<byte>(data[2..]);
+        Minutes    = MemoryMarshal.Read<ushort>(data[3..]);
     }
 
     [Flags]
     public enum TagUpdate : byte {
-        Time = 1,
-        State = 2
+        None      =  0,
+        Time      =  1,
+        State     =  2,
+        Both      =  3,
+        Unknown04 =  4,
+        Unknown05 =  5,
+        Unknown06 =  6,
+        Unknown07 =  7,
+        Unknown08 =  8,
+        Unknown09 =  9,
+        Unknown10 = 10,
+        Unknown11 = 11,
+        Unknown12 = 12,
+        Unknown13 = 13,
+        Unknown14 = 14,
+        All       = 15,
     }
+}
+
+public enum GameMode : sbyte {
+    None        = -1,
+    Legacy      =  0,
+    HideAndSeek =  1,
+    Sardines    =  2,
+    FreezeTag   =  3,
+    Unknown04   =  4,
+    Unknown05   =  5,
+    Unknown06   =  6,
+    Unknown07   =  7,
+    Unknown08   =  8,
+    Unknown09   =  9,
+    Unknown10   = 10,
+    Unknown11   = 11,
+    Unknown12   = 12,
+    Unknown13   = 13,
+    Reserved    = 14, // extension possibility for more future game modes with an extra added byte
 }
